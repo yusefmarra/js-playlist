0e3c8a81ba8b90bb99269a052678aba6bec3b9cf
yusefmarra <yusef.marra@gmail.com>
2015-08-04 14:59:06 -0600

    Submitted solutions for js-playlist exercises

--- playlist.js
+++ playlist.js
@@ -1,11 +1,66 @@
 var Song = require('./song');
 
-var Playlist = function(){
+var Playlist = function(songs){
+  this.songs = songs || [];
+  this.playing;
 };
 
 Playlist.prototype.isEmpty = function () {
+  if (this.songs.length){
+    return false;
+  } else {
+    return true;
+  }
 };
 
+Playlist.prototype.addSong = function(song){
+  this.songs.push(song);
+}
+
+Playlist.prototype.songNames = function(){
+  var songNames = [];
+  for (var i = 0; i < this.songs.length; i++) {
+    songNames.push(this.songs[i].name);
+  }
+  return songNames;
+}
+
+Playlist.prototype.removeSong = function(song){
+  this.songs.splice(this.songs.indexOf(song),1);
+}
+
+Playlist.prototype.totalLength = function(){
+  var total = 0;
+  for (var i = 0; i < this.songs.length; i++) {
+    total += this.songs[i].length;
+  }
+  return total;
+}
+
+Playlist.prototype.swap = function(first, second) {
+    var firstIndex = this.songs.indexOf(first);
+    this.songs[this.songs.indexOf(second)] = first;
+    this.songs[firstIndex] = second;
+}
+
+Playlist.prototype.nowPlaying = function() {
+  if (this.playing){
+    return this.playing;
+  } else {
+    return null;
+  }
+  // return this.playing;
+}
+
+Playlist.prototype.play = function() {
+  if (!this.playing) {
+    this.playing = this.songs[0];
+  }
+}
+
+Playlist.prototype.next = function() {
+  this.playing = this.songs[this.songs.indexOf(this.playing)+1]
+}
 
 Playlist.prototype.forNumber = function () {
 
--- spec/playlist_spec.js
+++ spec/playlist_spec.js
@@ -13,119 +13,119 @@ describe('Playlist', function() {
     expect(playlist.isEmpty()).toEqual(true);
   });
 
-  // it("allows you to add a song", function() {
-  //   var playlist = new Playlist();
-  // 
-  //   playlist.addSong(itHadToBeYou)
-  //
-  //   expect(playlist.isEmpty()).toEqual(false);
-  // });
+  it("allows you to add a song", function() {
+    var playlist = new Playlist();
 
-  // it("allows you to see all song names in the order they were added", function() {
-  //   var playlist = new Playlist();
-  //
-  //   expect(playlist.songNames()).toEqual([]);
-  //
-  //   playlist.addSong(itHadToBeYou)
-  //   expect(playlist.songNames()).toEqual(['It had to be you']);
-  //
-  //   playlist.addSong(butNotForMe)
-  //   expect(playlist.songNames()).toEqual([
-  //     'It had to be you',
-  //     'But Not for Me'
-  //   ]);
-  // });
+    playlist.addSong(itHadToBeYou)
 
-  // it("allows you remove a song from the playlist", function() {
-  //   var playlist = new Playlist();
-  //
-  //   playlist.addSong(itHadToBeYou);
-  //   expect(playlist.isEmpty()).toEqual(false);
-  //
-  //   playlist.addSong(butNotForMe);
-  //
-  //   expect(playlist.songNames()).toEqual([
-  //     'It had to be you',
-  //     'But Not for Me',
-  //   ]);
-  //
-  //   playlist.removeSong(itHadToBeYou)
-  //   expect(playlist.songNames()).toEqual([
-  //     'But Not for Me',
-  //   ]);
-  //   expect(playlist.isEmpty()).toEqual(false);
-  //
-  //   playlist.removeSong(butNotForMe)
-  //
-  //   expect(playlist.songNames()).toEqual([]);
-  //   expect(playlist.isEmpty()).toEqual(true);
-  // });
+    expect(playlist.isEmpty()).toEqual(false);
+  });
 
-  //
-  // it("tells you the total length of the playlist", function() {
-  //   var playlist = new Playlist();
-  //
-  //   playlist.addSong(itHadToBeYou);
-  //   expect(playlist.totalLength()).toEqual(100);
-  //
-  //   playlist.addSong(butNotForMe);
-  //   playlist.addSong(autumnInNewYork);
-  //
-  //   playlist.swap(itHadToBeYou, butNotForMe);
-  //   expect(playlist.totalLength()).toEqual(175);
-  // });
-  //
+  it("allows you to see all song names in the order they were added", function() {
+    var playlist = new Playlist();
 
-  // it("allows you to swap songs", function() {
-  //   var playlist = new Playlist();
-  //
-  //   playlist.addSong(itHadToBeYou);
-  //   playlist.addSong(butNotForMe);
-  //   playlist.addSong(autumnInNewYork);
-  //
-  //   playlist.swap(itHadToBeYou, butNotForMe);
-  //   expect(playlist.songNames()).toEqual([
-  //     'But Not for Me',
-  //     'It had to be you',
-  //     'Autumn In New York'
-  //   ]);
-  //
-  //   playlist.swap(itHadToBeYou, autumnInNewYork);
-  //   expect(playlist.songNames()).toEqual([
-  //     'But Not for Me',
-  //     'Autumn In New York',
-  //     'It had to be you'
-  //   ]);
-  // });
+    expect(playlist.songNames()).toEqual([]);
 
-  // it("allows you to play a song", function () {
-  //   var playlist = new Playlist();
-  //   playlist.addSong(itHadToBeYou);
-  //   playlist.addSong(butNotForMe);
-  //   playlist.addSong(autumnInNewYork);
-  //
-  //   expect(playlist.nowPlaying()).toEqual(null);
-  //
-  //   playlist.play();
-  //
-  //   expect(playlist.nowPlaying()).toEqual(itHadToBeYou);
-  // });
-  //
-  // it("allows you go to the next song", function () {
-  //   var playlist = new Playlist();
-  //   playlist.addSong(itHadToBeYou);
-  //   playlist.addSong(butNotForMe);
-  //   playlist.addSong(autumnInNewYork);
-  //
-  //   playlist.play();
-  //   playlist.next();
-  //   expect(playlist.nowPlaying()).toEqual(butNotForMe);
-  //
-  //   playlist.next();
-  //   expect(playlist.nowPlaying()).toEqual(autumnInNewYork);
-  //
-  //   playlist.next();
-  //   expect(playlist.nowPlaying()).toEqual(null);
-  // });
+    playlist.addSong(itHadToBeYou)
+    expect(playlist.songNames()).toEqual(['It had to be you']);
+
+    playlist.addSong(butNotForMe)
+    expect(playlist.songNames()).toEqual([
+      'It had to be you',
+      'But Not for Me'
+    ]);
+  });
+
+  it("allows you remove a song from the playlist", function() {
+    var playlist = new Playlist();
+
+    playlist.addSong(itHadToBeYou);
+    expect(playlist.isEmpty()).toEqual(false);
+
+    playlist.addSong(butNotForMe);
+
+    expect(playlist.songNames()).toEqual([
+      'It had to be you',
+      'But Not for Me',
+    ]);
+
+    playlist.removeSong(itHadToBeYou)
+    expect(playlist.songNames()).toEqual([
+      'But Not for Me',
+    ]);
+    expect(playlist.isEmpty()).toEqual(false);
+
+    playlist.removeSong(butNotForMe)
+
+    expect(playlist.songNames()).toEqual([]);
+    expect(playlist.isEmpty()).toEqual(true);
+  });
+
+
+  it("tells you the total length of the playlist", function() {
+    var playlist = new Playlist();
+
+    playlist.addSong(itHadToBeYou);
+    expect(playlist.totalLength()).toEqual(100);
+
+    playlist.addSong(butNotForMe);
+    playlist.addSong(autumnInNewYork);
+
+    playlist.swap(itHadToBeYou, butNotForMe);
+    expect(playlist.totalLength()).toEqual(175);
+  });
+
+
+  it("allows you to swap songs", function() {
+    var playlist = new Playlist();
+
+    playlist.addSong(itHadToBeYou);
+    playlist.addSong(butNotForMe);
+    playlist.addSong(autumnInNewYork);
+
+    playlist.swap(itHadToBeYou, butNotForMe);
+    expect(playlist.songNames()).toEqual([
+      'But Not for Me',
+      'It had to be you',
+      'Autumn In New York'
+    ]);
+
+    playlist.swap(itHadToBeYou, autumnInNewYork);
+    expect(playlist.songNames()).toEqual([
+      'But Not for Me',
+      'Autumn In New York',
+      'It had to be you'
+    ]);
+  });
+
+  it("allows you to play a song", function () {
+    var playlist = new Playlist();
+    playlist.addSong(itHadToBeYou);
+    playlist.addSong(butNotForMe);
+    playlist.addSong(autumnInNewYork);
+
+    expect(playlist.nowPlaying()).toEqual(null);
+
+    playlist.play();
+
+    expect(playlist.nowPlaying()).toEqual(itHadToBeYou);
+  });
+
+  it("allows you go to the next song", function () {
+    var playlist = new Playlist();
+    playlist.addSong(itHadToBeYou);
+    playlist.addSong(butNotForMe);
+    playlist.addSong(autumnInNewYork);
+  
+    playlist.play();
+    playlist.next();
+    expect(playlist.nowPlaying()).toEqual(butNotForMe);
+
+    playlist.next();
+    expect(playlist.nowPlaying()).toEqual(autumnInNewYork);
+
+    playlist.next();
+    expect(playlist.nowPlaying()).toEqual(null);
+  });
 
 });
